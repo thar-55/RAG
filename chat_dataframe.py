e5fa449a1e2b66b6445188affe25eecfefac2c56
thar-55 <thartestcomp@gmail.com>
2025-03-12 02:07:02 +0300

    Update chat_dataframe.py

--- chat_dataframe.py
+++ chat_dataframe.py
@@ -1,7 +1,7 @@
 # from langchain_experimental.agents import AgentType
-from langchain.agents import create_pandas_dataframe_agent
-# from langchain_experimental.agents import create_pandas_dataframe_agent
-
+# from langchain.agents import create_pandas_dataframe_agent
+from langchain_experimental.agents import create_pandas_dataframe_agent
+# langchain_experimental.agents.create_pandas_dataframe_agent
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
