20d3821c9ffbbe8b4419a8b23ab9701c3ff9ef9f
thar-55 <thartestcomp@gmail.com>
2025-03-14 02:35:41 +0300

    Update chat_dataframe.py

--- chat_dataframe.py
+++ chat_dataframe.py
@@ -175,7 +175,7 @@ if prompt := st.chat_input(placeholder="Enter the reference number "):
         st.stop()
 
     llm = ChatOpenAI(
-        temperature=0, model="gpt-4o", openai_api_key=openai_api_key, streaming=True
+        temperature=0.2, model="gpt-4o", openai_api_key=openai_api_key, streaming=True
     )
 
     pandas_df_agent = create_pandas_dataframe_agent(
