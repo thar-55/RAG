c6c410be73eb5d25564487e69dd898e64f552c67
thar-55 <thartestcomp@gmail.com>
2025-03-14 02:37:02 +0300

    Update chat_dataframe.py

--- chat_dataframe.py
+++ chat_dataframe.py
@@ -270,7 +270,7 @@ if prompt := st.chat_input(placeholder="Enter the reference number "):
         
         prompt_template = PromptTemplate( template="""user will enter a list of refrences in the input {input_refs} ,
         the user input will be maybe single string or a list of strings seprated by space or comma
-        for each input go and search column ref_id in the data attached  and return the records matching 
+        for each input go and search in ref_id data  
         each item in this list will contain:
         -customers who purchased the item with the ref_id
         -item title 
