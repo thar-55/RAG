45e85cf369eb34147f5aec1636d45213921fd660
thar-55 <thartestcomp@gmail.com>
2025-03-15 01:05:06 +0300

    Update chat_dataframe.py

--- chat_dataframe.py
+++ chat_dataframe.py
@@ -159,7 +159,7 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input(placeholder="Enter the reference number "):
-    df = pd.read_csv('beta_dataset_v2.csv')
+    # df = pd.read_csv('beta_dataset_v2.csv')
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
